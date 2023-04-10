a44d5b33bf5215cbd602d1f6e66cbb9b1b28fba0
Sarika Karle <104686761+sarikapanmand@users.noreply.github.com>
2023-04-10 20:34:53 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,22 @@ const app = require('./app');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
-
 dotenv.config();
-//connect to DB
-mongoose.connect(process.env.DATABASE_URL,{ useNewUrlParser: true, useUnifiedTopology: true }, () => {
-    console.log('connected to DB')
-})
 
+// Connect to MongoDB
+(async () => {
+  try {
+    await mongoose.connect(process.env.DATABASE_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+})();
 
-app.listen(3000, () => console.log('Server running......'));
-
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
